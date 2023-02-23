5bdec189d77818cc9e879bc3fb5887e64ed1dd2e
wodahS21 <yaelelyolo@gmail.com>
2023-02-23 09:58:25 -0700

    Agregar archivos de proyecto.

---
+++ PaintClass/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace PaintClass
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtR = new System.Windows.Forms.TextBox();
+            this.txtG = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.B = new System.Windows.Forms.Label();
+            this.txtPapel = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPapel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(27, 27);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox2.Location = new System.Drawing.Point(43, 11);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(27, 27);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(76, 11);
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(161, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "R";
+            // 
+            // txtR
+            // 
+            this.txtR.Location = new System.Drawing.Point(36, 85);
+            this.txtR.Name = "txtR";
+            this.txtR.Size = new System.Drawing.Size(100, 23);
+            this.txtR.TabIndex = 4;
+            // 
+            // txtG
+            // 
+            this.txtG.Location = new System.Drawing.Point(36, 126);
+            this.txtG.Name = "txtG";
+            this.txtG.Size = new System.Drawing.Size(100, 23);
+            this.txtG.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "G";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(36, 171);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 23);
+            this.txtB.TabIndex = 8;
+            // 
+            // B
+            // 
+            this.B.AutoSize = true;
+            this.B.Location = new System.Drawing.Point(10, 172);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(14, 15);
+            this.B.TabIndex = 7;
+            this.B.Text = "R";
+            // 
+            // txtPapel
+            // 
+            this.txtPapel.Location = new System.Drawing.Point(171, 67);
+            this.txtPapel.Name = "txtPapel";
+            this.txtPapel.Size = new System.Drawing.Size(617, 371);
+            this.txtPapel.TabIndex = 9;
+            this.txtPapel.TabStop = false;
+            this.txtPapel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.txtPapel_MouseDown);
+            this.txtPapel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.txtPapel_MouseMove);
+            this.txtPapel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.txtPapel_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPapel);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.txtG);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtR);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPapel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private TrackBar trackBar1;
+        private Label label1;
+        private TextBox txtR;
+        private TextBox txtG;
+        private Label label2;
+        private TextBox txtB;
+        private Label B;
+        private PictureBox txtPapel;
+    }
+}
---
+++ PaintClass/Form1.cs
@@ -0,0 +1,56 @@
+namespace PaintClass
+{
+    public partial class Form1 : Form
+    {
+        Graphics papel;
+        int anchoPluma = 3;
+        int x = 0, y = 0;
+        bool moving;
+        Pen pen;
+        byte r = 0;
+        byte g = 0;
+        byte b = 0;
+        public Form1()
+        {
+          
+            InitializeComponent();
+            papel = txtPapel.CreateGraphics();
+            pen = new Pen(Color.Black, anchoPluma);
+            //Cuando se dibuje en el pictureBox se suavice el trazo
+            papel.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            anchoPluma = trackBar1.Value;
+
+        }
+
+        private void txtPapel_MouseDown(object sender, MouseEventArgs e)
+        {
+            moving = true;
+            x=e.X; y=e.Y;
+        }
+
+        private void txtPapel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (moving)
+            {
+                papel.DrawLine(pen, new Point(x, y), e.Location);
+                x = e.X; y= e.Y;
+                cambiarValorPluma();
+            }
+        }
+
+        private void txtPapel_MouseUp(object sender, MouseEventArgs e)
+        {
+            moving = false;
+        }
+
+        private void cambiarValorPluma()
+        {
+            pen = new Pen(Color.Black, anchoPluma);
+            pen.StartCap = pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+        }
+    }
+}
